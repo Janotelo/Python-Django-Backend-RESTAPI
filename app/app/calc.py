331c96fbf69e59f4597b743248ef0a4f41fa6501
Janotelo <janos.jantoc@gmail.com>
2023-11-20 21:45:12 +0800

    Section 8-40to41 | Added wait_for_db command | Fixed Linting [skip ci]

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,6 +7,7 @@ def add(x, y):
     """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
     """Subtract x from y and return results."""
     return y - x
--- app/app/test.py
+++ app/app/test.py
@@ -9,7 +9,6 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
-
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
