3a7aab4b2ddcd3285ededbb88259965d8224cfae
Janotelo <janos.jantoc@gmail.com>
2023-11-21 15:04:44 +0800

    Section 9-51 - Add superuser support | Added test and function for creating superuser [skip ci]

--- app/core/models.py
+++ app/core/models.py
@@ -21,7 +21,15 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
