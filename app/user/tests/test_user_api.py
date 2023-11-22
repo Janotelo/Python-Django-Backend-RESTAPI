e6302915c653eb54ae1cc9d99322f19ee545802b
Janotelo <janos.jantoc@gmail.com>
2023-11-22 11:13:50 +0800

    Section 12-73 - Implement manage user API | Added API for managing users | Fixed error in test_user_api.py [skip ci]

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -131,7 +131,7 @@ class PrivateUserApiTests(TestCase):
 
     def test_post_me_not_allowed(self):
         """Test POST is not allowed for the me endpoint."""
-        res = self.cleint.post(ME_URL, {})
+        res = self.client.post(ME_URL, {})
 
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
     
