d3ab5f1213cf2c4fed12fa14d53bbcda8f1f71cb
Janos Angelo Garcia Jantoc <janos.jantoc@gmail.com>
2023-11-26 23:30:34 +0800

    Section 15-106 - Add ingredient model | Added test for creating ingredient and made the model to make it pass [skip ci]

--- app/core/models.py
+++ app/core/models.py
@@ -56,6 +56,7 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
@@ -67,6 +68,18 @@ class Tag(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredients for recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
     )
 
     def __str__(self):
